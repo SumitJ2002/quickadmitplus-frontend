27e3500e1edce3f8e4cbc05d44ed11d2ee6bb7a2
SumitJ2002 <sumitjaiswarsj420@gmail.com>
2024-08-09 16:53:44 +0530

    first commit

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,11 +1,13 @@
-import axios from 'axios';
+import axios from "axios";
 
 const http = axios.create({
-    baseURL:"https://quickadmitplus.onrender.com",
-    headers:{
-        'jwtoken':document.cookie.replace(/(?:(?:^|.*;\s*)jwtoken\s*=\s*([^;]*).*$)|^.*$/, '$1')
-        
-    }
-})
+  baseURL: "http://localhost:8000",
+  headers: {
+    jwtoken: document.cookie.replace(
+      /(?:(?:^|.*;\s*)jwtoken\s*=\s*([^;]*).*$)|^.*$/,
+      "$1"
+    ),
+  },
+});
 
 export default http;
